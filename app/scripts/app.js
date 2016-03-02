f114add4ee3d80aca89a8f6380923faa8734e376
vishal pradhan <vishu_vishal_bi@live.com>
2016-03-03 01:41:24 +0530

    testcommit1

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bootstrapApp
+ * @description
+ * # bootstrapApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bootstrapApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bootstrapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bootstrapApp
+ */
+angular.module('bootstrapApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
